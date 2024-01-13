788828de0bf360ead2854fb208f2703186f83878
rishithaiiitb <Himarishitha.Kakunuri@iiitb.ac.in>
2024-01-13 16:40:17 +0530

    by b2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,9 @@
-//HelloWorld
+//HelloWorld b2
 public class HelloWorld { 
 	public static void main(String args[]) 
 	{ 
 		System.out.println("Hello, World from Master Branch !"); 
+		System.out.println("by b2");
 		System.exit(0);
 	} 
 }
